a3b59224bb65da3d4a493164357bf86340e0c906
Myron Huang <myron.h93@gmail.com>
2020-05-24 23:53:53 -0400

    Updates

---
+++ precache-manifest.d58fa180fa48d0dc078d773b33b28ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce85a8d6150372eef56bacb536f15bfe",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "0c21fdd0c4659752dfad",
+    "url": "/todo-list/static/css/main.c5d2e8f7.chunk.css"
+  },
+  {
+    "revision": "1ea1c792941d7611ddb4",
+    "url": "/todo-list/static/js/2.99ccce82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.99ccce82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c21fdd0c4659752dfad",
+    "url": "/todo-list/static/js/main.b75502fe.chunk.js"
+  },
+  {
+    "revision": "141db23f0bfdce7683c3",
+    "url": "/todo-list/static/js/runtime-main.ec90df12.js"
+  }
+]);
